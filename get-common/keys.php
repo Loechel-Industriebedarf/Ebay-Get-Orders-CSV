b84c4c20ce54af3c56fb2dbf3a5d7d6373351802
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2020-01-09 13:15:00 +0100

    Updated $compatabilityLevel to work with new order numbers

--- get-common/keys.php
+++ get-common/keys.php
@@ -1,5 +1,5 @@
 <?php
-/*  © 2013 eBay Inc., All Rights Reserved */ 
+/*  Â© 2013 eBay Inc., All Rights Reserved */ 
 /* Licensed under CDDL 1.0 -  http://opensource.org/licenses/cddl1.php */
     //show all errors
     error_reporting(E_ALL);
@@ -7,7 +7,7 @@
     // these keys can be obtained by registering at http://developer.ebay.com
     
     $production         = true;   // toggle to true if going against production
-    $compatabilityLevel = 717;    // eBay API version
+    $compatabilityLevel = 1113;    // eBay API version
     
     if ($production) {
         $devID = 'xxxxx';   // these prod keys are different from sandbox keys
